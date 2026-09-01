b2d159ab02e4ae723da67b5e5775cb536c9a5d0d
achintha-sh <achinthas2000@gmail.com>
2026-09-01 20:32:12 +0530

    added to splash screen

--- app/src/main/java/com/achinthas/infoflow/MainActivity.kt
+++ app/src/main/java/com/achinthas/infoflow/MainActivity.kt
@@ -3,11 +3,16 @@ package com.achinthas.infoflow
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // splash screen implements
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
