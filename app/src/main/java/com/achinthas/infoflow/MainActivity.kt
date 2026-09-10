87a80a0b578fc012825b6c88533b24ef1d2001e2
achintha-sh <achinthas2000@gmail.com>
2026-09-11 02:44:32 +0530

    add bottom navigation

--- app/src/main/java/com/achinthas/infoflow/MainActivity.kt
+++ app/src/main/java/com/achinthas/infoflow/MainActivity.kt
@@ -8,16 +8,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.achinthas.infoflow.onboarding.OnboardingActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         // splash screen implements
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -29,11 +34,41 @@ class MainActivity : AppCompatActivity() {
         val sharedPref = getSharedPreferences("app_pref", Context.MODE_PRIVATE)
         val isCompleted = sharedPref.getBoolean("is_onboarding_completed", false)
 
-        if(!isCompleted){
+        if (!isCompleted) {
             startActivity(Intent(this, OnboardingActivity::class.java))
             finish()
             return
         }
+
+
+        bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.bottom_home -> {
+                    replaceFragment(HomeViewFragment())
+                    true
+                }
+
+                R.id.bottom_notification -> {
+                    replaceFragment(NotificationViewFragment())
+                    true
+                }
+
+                R.id.bottom_settings -> {
+                    replaceFragment(SettingsViewFragment())
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        replaceFragment(HomeViewFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
 
     }
 }
